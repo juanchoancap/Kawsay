c2e0d9ac4300be2cf77262a3ab8ad9288e16f408
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2021-04-16 03:26:47 -0300

    all the components

---
+++ client/src/calculator/Start.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Start() {
+  return (
+    <div>
+      <h3>¡Bienvenido a Kawsay!</h3>
+      <div>
+        <h2>La huella del Carbono</h2>
+        <h1>
+          ¿Sabías que con nuestro estilo de vida moderno, cada persona genera
+          alrededor de cuatro toneladas de CO2 al año?
+        </h1>
+        <h1>
+          Una de las principales causas del calentamiento global es la emisión
+          de gases de efecto invernadero, entre ellos y principalmente, el CO2.
+          La marca que se deja sobre el medio ambiente con cada actividad que
+          emite gases de efecto invernadero es conocida como huella de carbono.
+        </h1>
+        <h1>¿Te gustaria conocer tu huella de Carbono?</h1>
+        <div>
+          <button>Calcular</button>
+        </div>
+      </div>
+    </div>
+  );
+}
