93d6f6bbf329e75210fef4a015641a5149fa108c
Joaquin Palacios <joa_palacios@hotmail.com>
2021-04-21 08:50:40 +1000

    first commit

---
+++ client/src/Container/container.jsx
@@ -0,0 +1,12 @@
+import Question1 from "../calculator/Question1"
+
+const questionsListContainer = () => {
+    return (
+        <>
+        {/* <Question1 /> */}
+        </>
+    )
+
+};
+
+export default questionsListContainer;
