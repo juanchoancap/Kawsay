2d6b4e2f48838d8f719356ff6e3fb1f21eb13a58
SilvinaZB <zb.silvina@gmail.com>
2021-04-24 01:02:56 -0300

    probando

--- client/src/Components/NavBar/index.jsx
+++ client/src/Components/NavBar/index.jsx
@@ -1,31 +1,32 @@
 import { Navbar as NavbarBootstrap } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
+import style from "../../CSS/App.css"
 
 const NavBar = () => {
   const { push } = useHistory();
   return (
     <div>
-      <NavbarBootstrap bg="info" variant="success">
+      <NavbarBootstrap bg="white" variant="success">
         <NavbarBootstrap.Brand className=" font-weight-bold mx-5 text-muted">
           Kawsai
         </NavbarBootstrap.Brand>
       </NavbarBootstrap>
-      <div>
+      <div className = "ponele">
         <ul>
           <li>
-            <Link to="/">Calculadora</Link>
+            <Link to="/"><h5 className={style.color}>Calculadora</h5></Link>
           </li>
           <li>
-            <Link to="/consejos">Mejora tu impacto</Link>
+            <Link to="/consejos"><h5 className={style.color}>Mejora tu impacto</h5></Link>
+          </li>
+          <li>
+            <Link to="/planta"><h5 className={style.color}>Planta un arbol</h5></Link>
+          </li>
+          <li>
+            <Link to="/newsletter"><h5 className={style.color}>Suscribete</h5></Link>
           </li>
         </ul>
       </div>
-      <li>
-        <Link to="/planta">Planta un Arbol</Link>
-      </li>
-      <li>
-        <Link to="/newsletter">Suscribete</Link>
-      </li>
     </div>
   );
 };
