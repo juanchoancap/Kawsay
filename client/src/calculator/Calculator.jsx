9077a0b33f1e920e28d10d947c9bda0c443f7cdc
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2021-04-29 00:14:21 -0300

    calculator fix

--- client/src/calculator/Calculator.jsx
+++ client/src/calculator/Calculator.jsx
@@ -198,67 +198,51 @@ export default function Calculator() {
 
   return (
     <>
-
-    <Jumbotron className ="cover" style={{backgroundImage: `url("http://i.ibb.co/jgZph3V/fondo.png")`}}>
-      {/* start */}
-      {location === "start" ? (
-        <div>
+      <Jumbotron
+        className="cover"
+        style={{ backgroundImage: `url("http://i.ibb.co/jgZph3V/fondo.png")` }}
+      >
+        {/* start */}
+        {location === "start" ? (
           <div>
-            <h1 className="my-2 parH text-left font-weight-bold pl-2"><span className = "titleH">¡Bienvenido a </span>Kawsay!</h1>
+            <div>
+              <h1 className="my-2 parH text-left font-weight-bold pl-2">
+                <span className="titleH">¡Bienvenido a </span>Kawsay!
+              </h1>
+            </div>
+            <div className="shadow text-left w-50 parHB">
+              <p className="parH mt-5 pl-2 pt-2">
+                <h5>
+                  ¿Sabías que con nuestro estilo de vida moderno, cada persona
+                  genera alrededor de cuatro toneladas de CO2 al año?
+                </h5>
+              </p>
+              <p className="parH pb-3 pl-2">
+                Una de las principales causas del calentamiento global es la
+                emisión de gases de efecto invernadero, entre ellos y
+                principalmente, el CO2. La marca que se deja sobre el medio
+                ambiente con cada actividad que emite gases de efecto
+                invernadero es conocida como huella de carbono.
+              </p>
+            </div>
+            <div>
+              <p className="parH mb-n3 mt-3 py-3 pl-2">
+                <h5 className="font-weight-bold">
+                  ¿Te gustaria conocer tu huella de Carbono?
+                </h5>
+              </p>
+              <div className="ml-5 mt-2">
+                <Button variant="flat" size="lg" onClick={() => handleButton()}>
+                  CALCULAR
+                </Button>
+              </div>
+            </div>
           </div>
-          <div className="shadow text-left w-50 parHB">
-            <p className="parH mt-5 pl-2 pt-2">
-              <h5>¿Sabías que con nuestro estilo de vida moderno, cada persona
-              genera alrededor de cuatro toneladas de CO2 al año?</h5>
-            </p>
-            <p className="parH pb-3 pl-2">
-              Una de las principales causas del calentamiento global es la
-              emisión de gases de efecto invernadero, entre ellos y
-              principalmente, el CO2. La marca que se deja sobre el medio
-              ambiente con cada actividad que emite gases de efecto invernadero
-              es conocida como huella de carbono.
-            </p>
-          </div>
-          <div>
-            <p className="parH mb-n3 mt-3 py-3 pl-2">
-              <h5 className="font-weight-bold">¿Te gustaria conocer tu huella de Carbono?</h5>
-            </p>
-            <div className="ml-5 mt-2">
-              <Button variant="flat" size="lg" onClick={() => handleButton()}>
-                CALCULAR
-              </Button>
-          </div>
-          </div>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question1 */}
-      {location === "question1" ? (
-        <div>
-          <h2 className="font-italic text-muted">¿Con cuanta gente vives?</h2>
-          <input
-            type="number"
-            name="people"
-            min="1"
-            value={input.people}
-            onChange={handleInput}
-            placeholder="Ingrese la cantidad"
-          />
-          <br />
-          <Button className="my-2" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button className="mt-1" variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question2 */}
-      {location === "question2" ? (
-        <div className="font-italic text-muted">
-          <h2>¿Cuales de estos electrodomesticos utilizas?</h2>
-          <h4 className="mb-4">(puede marcar tantos como quiera)</h4>
-
+        ) : (
+          <div />
+        )}
+        {/* question1 */}
+        {location === "question1" ? (
           <div>
             <h2 className="font-italic text-muted">¿Con cuanta gente vives?</h2>
             <input
@@ -272,7 +256,7 @@ export default function Calculator() {
             <br />
             <Button
               className="my-2"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -281,7 +265,7 @@ export default function Calculator() {
             <br />
             <Button
               className="mt-1"
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
@@ -390,7 +374,7 @@ export default function Calculator() {
             </div>
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -398,7 +382,7 @@ export default function Calculator() {
             </Button>
             <br />
             <Button
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
@@ -453,7 +437,7 @@ export default function Calculator() {
             </div>
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -461,28 +445,20 @@ export default function Calculator() {
             </Button>
             <br />
             <Button
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
               Volver al Inicio
             </Button>
           </div>
-
-          <Button className="my-3" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question 3 */}
-      {location === "question3" ? (
-        <div className="font-italic text-muted">
-          <h2>¿Cuales de estos utilizas con frecuencia?</h2>
-          <h4 className="mb-4">(puede marcar tantos como quiera)</h4>
-          <div>
-
+        ) : (
+          <div />
+        )}
+        {/* question 4 */}
+        {location === "question4" ? (
+          <div className="font-italic text-muted">
+            <h2 className="mb-4">¿Tenes vehiculo propio?</h2>
             <input
               type="checkbox"
               name="useCar"
@@ -492,11 +468,10 @@ export default function Calculator() {
               onClick={() => setInput({ ...input, useCar: !input.useCar })}
               className="mx-2"
             />
-            <h6>(Marca SOLO si tienes vehiculo propio)</h6>
             <br />
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -504,7 +479,7 @@ export default function Calculator() {
             </Button>
             <br />
             <Button
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
@@ -529,7 +504,7 @@ export default function Calculator() {
             <br />
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -537,7 +512,7 @@ export default function Calculator() {
             </Button>
             <br />
             <Button
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
@@ -564,7 +539,7 @@ export default function Calculator() {
             <br />
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -572,111 +547,20 @@ export default function Calculator() {
             </Button>
             <br />
             <Button
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
               Volver al Inicio
             </Button>
           </div>
-
-          <Button className="my-3" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question 4 */}
-      {location === "question4" ? (
-        <div className="font-italic text-muted">
-          <h2 className="mb-4">¿Tenes vehiculo propio?</h2>
-          <input
-            type="checkbox"
-            name="useCar"
-            value={input.useCar}
-            checked={input.useCar}
-            defaultChecked={false}
-            onClick={() => setInput({ ...input, useCar: !input.useCar })}
-            className="mx-2"
-          />
-          <br />
-          <Button className="my-3" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question 4A */}
-      {location === "question4A" ? (
-        <div className="font-italic text-muted">
-          <h2 className="mb-4">¿Que distancia recorres aprox. al mes?</h2>
-          <input
-            type="number"
-            name="carDistance"
-            min="1"
-            value={input.carDistance}
-            onChange={handleInput}
-            placeholder="Ingrese la cantidad"
-          />
-          <br />
-          <Button className="my-3" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question 4B */}
-      {location === "question4B" ? (
-        <div className="font-italic text-muted">
-          <h2 className="mb-4">¿Cuantas personas comparten tu auto habitualmente?</h2>
-          <input
-            type="number"
-            name="carShare"
-            min="1"
-            value={input.carShare}
-            onChange={handleInput}
-            placeholder="Ingrese la cantidad"
-          />
-          <br />
-          <Button className="my-3" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question 5 */}
-      {location === "question5" ? (
-        <div className="font-italic text-muted">
-          <h2 className="mb-4">¿Usas con frecuencia transporte publico?</h2>
-          <input
-            type="checkbox"
-            name="publicTransport"
-            value={input.publicTransport}
-            checked={input.publicTransport}
-            defaultChecked={false}
-            onClick={() =>
-              setInput({ ...input, publicTransport: !input.publicTransport })
-            }
-          />
-          <br />
-          <Button className="my-3" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question 5A */}
-      {location === "question5A" ? (
-        <div className="font-italic text-muted">
-          <h2>¿Cuales de estos transportes utilizas con frecuencia?</h2>
-          <h4 className="mb-4">(puede marcar tantos como quiera)</h4>
-          <div>
-
+        ) : (
+          <div />
+        )}
+        {/* question 5 */}
+        {location === "question5" ? (
+          <div className="font-italic text-muted">
+            <h2 className="mb-4">¿Usas con frecuencia transporte publico?</h2>
             <input
               type="checkbox"
               name="publicTransport"
@@ -690,7 +574,7 @@ export default function Calculator() {
             <br />
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -698,7 +582,7 @@ export default function Calculator() {
             </Button>
             <br />
             <Button
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
@@ -751,7 +635,7 @@ export default function Calculator() {
             </div>
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -759,7 +643,7 @@ export default function Calculator() {
             </Button>
             <br />
             <Button
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
@@ -786,7 +670,7 @@ export default function Calculator() {
             <br />
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               size="lg"
               onClick={() => handleButton()}
             >
@@ -794,7 +678,7 @@ export default function Calculator() {
             </Button>
             <br />
             <Button
-              variant="success"
+              variant="flat"
               size="sm"
               onClick={() => setLocation("start")}
             >
@@ -809,50 +693,16 @@ export default function Calculator() {
             <h3 className="mb-4">El Resultado es: {input.total}</h3>
             <Button
               className="my-3"
-              variant="success"
+              variant="flat"
               onClick={() => handleButton()}
             >
               Reiniciar
             </Button>
           </div>
-
-          <Button className="my-3" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {/* question 5B */}
-      {location === "question5B" ? (
-        <div className="font-italic text-muted">
-          <h2 className="mb-4">¿Cuantas horas diarias viajas en transporte publico?</h2>
-          <input
-            type="number"
-            name="publicHours"
-            min="1"
-            value={input.publicHours}
-            onChange={handleInput}
-            placeholder="Ingrese la cantidad"
-          />
-          <br />
-          <Button className="my-3" variant="flat" size="lg" onClick={() => handleButton()}>Continuar</Button>
-          <br />
-          <Button variant="flat" size="sm" onClick={() => setLocation("start")}>Volver al Inicio</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-      {location === "result" ? (
-        <div className="font-italic text-muted">
-          <h3 className="mb-4">El Resultado es: {input.total}</h3>
-          <Button className="my-3" variant="flat" onClick={() => handleButton()}>Reiniciar</Button>
-        </div>
-      ) : (
-        <div />
-      )}
-    </Jumbotron>
-
+        ) : (
+          <div />
+        )}
+      </Jumbotron>
     </>
   );
 }
