dc733ae5b2c1ce45534ac1e189b9d0f8010fde68
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2021-07-29 00:19:16 -0300

    fdkaofak

--- client/src/Donate.jsx
+++ client/src/Donate.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function Donate() {
   return (
-  <>
-  <div className="cover font-italic text-muted"
-      style={{ backgroundImage: `url("http://i.ibb.co/jgZph3V/fondo.png")` }}
-    >
-  <div>aprende algo dinero</div>
-  </div>
-  </>
+    <>
+      <div
+        className="cover font-italic text-muted"
+        style={{ backgroundImage: `url("http://i.ibb.co/jgZph3V/fondo.png")` }}
+      >
+        <div>Texto para que la gente Done o se suscriba</div>
+      </div>
+    </>
   );
 }
--- client/src/calculator/Calculator.jsx
+++ client/src/calculator/Calculator.jsx
@@ -173,8 +173,9 @@ export default function Calculator() {
         totalPublic += input.total;
         setInput({
           ...input,
-          total: totalPublic,
+          total: totalPublic.toFix(2),
         });
+
         setLocation("result");
         break;
       default:
@@ -690,14 +691,42 @@ export default function Calculator() {
         )}
         {location === "result" ? (
           <div className="font-italic text-muted">
-            <h3 className="mb-4">El Resultado es: {input.total}</h3>
-            <Button
-              className="my-3"
-              variant="flat"
-              onClick={() => handleButton()}
-            >
-              Reiniciar
-            </Button>
+            <h3 className="mb-4">
+              El Resultado es: {input.total} Toneladas anuales de emision de
+              huella de carbono.
+            </h3>
+            <p className="parH pb-3 pl-2">
+              <h6 className="font-weight-bold ">
+                ¿Sabías que los árboles funcionan como termorreguladores
+                capturando estas emisiones de co2?
+              </h6>{" "}
+              Se calcula que una hectárea de bosque mayor a 20 años puede llegar
+              a capturar las emisiones generadas por una persona por año.
+              <div>
+                <p className="parH mb-n3 mt-3 py-3 pl-2">
+                  <h5 className="font-weight-bold">
+                    ¿Qué esperas para mitigar tu huella de carbono? Podés
+                    empezar ahora.
+                  </h5>
+                </p>
+                <div className="ml-5 mt-2">
+                  <Button
+                    variant="flat"
+                    size="lg"
+                    onClick={() => handleButton()}
+                  >
+                    Siguiente
+                  </Button>
+                  <Button
+                    size="lg"
+                    variant="flat"
+                    onClick={() => handleButton()}
+                  >
+                    Reiniciar
+                  </Button>
+                </div>
+              </div>
+            </p>
           </div>
         ) : (
           <div />
